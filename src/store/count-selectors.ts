8b9d6bec9f154ce739bca276647b0466a93605fc
zhankaam <zhankaam@gmail.com>
2021-04-04 18:12:10 +0300

    incubator exam

---
+++ src/store/count-selectors.ts
@@ -0,0 +1,14 @@
+import {RootStateType} from "./store";
+//
+// export const getCount = (state: RootStateType) => {
+//     return state.count.count
+// }
+// export const getMaxValue = (state: RootStateType) => {
+//     return state.count.maxValue
+// }
+// export const getStartValue = (state: RootStateType) => {
+//     return state.count.startValue
+// }
+// export const getError = (state: RootStateType) => {
+//     return state.count.error
+// }
